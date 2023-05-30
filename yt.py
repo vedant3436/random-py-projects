891f01e4beb06179f0582f20d3be8019640b9a16
Vedant Mahanavar <mahanavarvedant7049@gmail.com>
2023-05-30 21:43:51 +0530

    Update yt.py

--- yt.py
+++ yt.py
@@ -1,10 +1,11 @@
+# pip install pytube before executing
 from pytube import YouTube
 while True:
     try:
         ytlink = input("Paste the link: ")
         yt_video = YouTube(ytlink)
-        videos = yt_video.streams.all()
-        vid = list(enumerate(videos))
+        videos = yt_video.streams.all() #list of all available types/resolutions for download
+        vid = list(enumerate(videos)) #easier for user to pick from the options
         for i in vid:
             print(i)
 
